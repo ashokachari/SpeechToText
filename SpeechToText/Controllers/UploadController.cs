255ea84722101d2d9838ccab40f3d9957da42bc9
Ashoka Chari Guntoju <ashokachari.g@gmail.com>
2024-02-24 12:27:19 +0530

    UI Changes

--- SpeechToText/Controllers/UploadController.cs
+++ SpeechToText/Controllers/UploadController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SpeechToText.Controllers
 {
@@ -10,16 +14,13 @@ namespace SpeechToText.Controllers
         {
             return View();
         }
-
         [HttpPost]
-        [HttpPost]
-        public async Task<IActionResult> UploadFile(IFormFile file)
+        public async Task<IActionResult> UploadFile(IFormFile file, string transcriptionMode)
         {
             try
             {
                 string apiUrl = "http://127.0.0.1:5000/upload"; // Replace with your API endpoint
-
-                var completeFilePath="D:\\"+file.FileName;
+                var completeFilePath = "D:\\" + file.FileName;
 
                 using (var client = new HttpClient())
                 using (var content = new MultipartFormDataContent())
@@ -32,11 +33,28 @@ namespace SpeechToText.Controllers
 
                     if (response.IsSuccessStatusCode)
                     {
-                        // Successfully uploaded
                         var data = await response.Content.ReadAsStringAsync();
-
                         JObject jsonObject = JObject.Parse(data);
 
+                        var textContent = jsonObject["transcription"];
+
+                        if (transcriptionMode == "file")
+                        {
+                            // If transcriptionMode is "file", download the file
+                            var textFileName = "transcription.txt";
+                            var textBytes = Encoding.UTF8.GetBytes(textContent.ToString());
+                            var textStream = new MemoryStream(textBytes);
+
+                            // Create a text file response
+                            var fileResult = File(textStream, "text/plain", textFileName);
+
+                            // Assign transcription data to ViewBag
+                            ViewBag.Message = jsonObject["transcription"];
+
+                            return fileResult;
+                        }
+
+                        // If transcriptionMode is "inline", assign transcription data to ViewBag
                         ViewBag.Message = jsonObject["transcription"];
                     }
                     else
@@ -53,7 +71,6 @@ namespace SpeechToText.Controllers
 
             return View("Index");
         }
-
 
     }
 }
